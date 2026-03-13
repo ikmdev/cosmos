886536f989e35b8c977b78ce0fb812df65b30d98
Andrew K Sills <andrew.k.sills@gmail.com>
2026-03-12 20:41:54 -0400

    Removes unused configuration annotation

--- src/main/java/dev/ikm/server/Application.java
+++ src/main/java/dev/ikm/server/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(DatabaseConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
